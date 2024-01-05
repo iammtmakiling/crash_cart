04cd5287d7d2df9cd51d767cbe2e3015f9c44fb4
iammtmakiling <mjaymakiling@gmail.com>
2024-01-05 14:30:42 +0800

    ui: stream appbar

--- my_app/lib/src/tabs/streamTab.dart
+++ my_app/lib/src/tabs/streamTab.dart
@@ -6,22 +6,102 @@ class StreamTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // Number of tabs
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tabs Example'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Stream'),
-              Tab(text: 'History'),
-            ],
+    return Column(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Spacer(),
+                    PopupMenuButton<String>(
+                      icon: const Icon(Icons.menu, color: Colors.grey),
+                      itemBuilder: (context) => [
+                        const PopupMenuItem<String>(
+                          value: 'logout',
+                          child: Text('Logout'),
+                        ),
+                      ],
+                      onSelected: (value) {
+                        if (value == 'logout') {
+                          // Implement logout functionality
+                          // For example, you can navigate to the login screen
+                          Navigator.pushReplacementNamed(context, '/login');
+                        }
+                      },
+                    ),
+                  ],
+                ),
+                const Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Crash Cart',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.cyan,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20)
+              ],
+            ),
           ),
         ),
-        body: const TabBarView(
-          children: [SubStreamTab(), SubHistoryTab()],
+        Expanded(
+          child: DefaultTabController(
+            length: 2, // Number of tabs
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.white,
+                toolbarHeight: 10,
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(
+                      child: Text(
+                        'Stream',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.cyan,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Tab(
+                      child: Text(
+                        'History',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.cyan,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              body: const TabBarView(
+                children: [
+                  SubStreamTab(),
+                  SubHistoryTab(),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
